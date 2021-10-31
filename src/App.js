bc3ce2d8fb082ba6788c49dbc2609af4990e533f
Ahsan Habib <ahsanhabibseo@gmail.com>
2021-10-31 07:02:41 -0700

    added-all-functionality

--- src/App.js
+++ src/App.js
@@ -13,41 +13,68 @@ import MyOrder from './components/MyOrder/MyOrder';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import ManageAllOrder from './components/ManageAllOrder/ManageAllOrder';
 import AddNewService from './components/AddNewService/AddNewService';
+import NotFound from './components/NotFound/NotFound';
+import { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-          <Header></Header>
-          <Switch>
-            <Route exact path="/">
-              <Banner></Banner>
-              <Benefit></Benefit>
-              <Services></Services>
-              <Newsletter></Newsletter>
-              <Footer></Footer>
-            </Route>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <PrivateRoute exact path="/services/:id">
-              <Order></Order>
-            </PrivateRoute>
-            <PrivateRoute exact path="/myorder">
-              <MyOrder></MyOrder>
-            </PrivateRoute>
-            <PrivateRoute exact path="/manageallorder">
-              <ManageAllOrder></ManageAllOrder>
-            </PrivateRoute>
-            <PrivateRoute exact path="/addnewservice">
-              <AddNewService></AddNewService>
-            </PrivateRoute>
-          </Switch>
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
-  );
-}
+  // Set loading state to true initially
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      await new Promise((r) => setTimeout(r, 2000));
+      setLoading((loading) => !loading);
+    };
+
+    loadData();
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <Spinner animation="grow" />
+      </div>
+    )
+  }
+  else{
+    return (
+      <div className="App">
+        <AuthProvider>
+          <BrowserRouter>
+            <Header></Header>
+            <Switch>
+              <Route exact path="/">
+                <Banner></Banner>
+                <Benefit></Benefit>
+                <Services></Services>
+                <Newsletter></Newsletter>
+                <Footer></Footer>
+              </Route>
+              <Route path="/login">
+                <Login></Login>
+              </Route>
+              <PrivateRoute exact path="/services/:id">
+                <Order></Order>
+              </PrivateRoute>
+              <PrivateRoute exact path="/myorder">
+                <MyOrder></MyOrder>
+              </PrivateRoute>
+              <PrivateRoute exact path="/manageallorder">
+                <ManageAllOrder></ManageAllOrder>
+              </PrivateRoute>
+              <PrivateRoute exact path="/addnewservice">
+                <AddNewService></AddNewService>
+              </PrivateRoute>
+              <Route path="*">
+                <NotFound></NotFound>
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </AuthProvider>
+      </div>
+    );
+  }
+  }
 
 export default App;
